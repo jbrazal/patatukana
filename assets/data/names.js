c2203649a6d2bb5fb616e4ba45866203c0adbcb2
Jed Brazal <42727623+jbrazal@users.noreply.github.com>
2018-09-30 10:32:32 -0600

    Update names.js with populated x_names

--- assets/data/names.js
+++ assets/data/names.js
@@ -1,1 +1,3 @@
-    var allNames = a_names.concat(b_names, c_names, d_names, e_names, f_names, g_names, h_names, i_names, j_names, k_names, l_names, m_names, n_names, o_names, p_names, q_names, r_names, s_names, t_names, u_names, v_names, w_names, x_names, y_names, z_names);
+    var allNames = a_names.concat(b_names, c_names, d_names, e_names, f_names, g_names, h_names, i_names, j_names, k_names
+        //, l_names, m_names, n_names, o_names, p_names, q_names, r_names, s_names, t_names, u_names, v_names, w_names, x_names, y_names, z_names
+        );
