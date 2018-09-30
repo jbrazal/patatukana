c637ea25a169e3a272de4b2d4c6c2f12cd51cd99
Jed Brazal <42727623+jbrazal@users.noreply.github.com>
2018-09-30 10:18:36 -0600

    Add search.js

---
+++ assets/scripts/search.js
@@ -0,0 +1,15 @@
+var lsearch = function(){
+    //this.term = document.getElementById('S').value.toUpperCase();
+    var s = document.getElementById('search').value.toLowerCase();
+    rows = document.getElementById('data').getElementsByTagName('tr');
+    for(var i=0;i<rows.length;i++){
+      if (s =="" ){
+        rows[i].style.display ='';
+      } else if ( rows[i].innerText.toLowerCase().indexOf(s) != -1 ) {
+        rows[i].style.display ='';
+      } else {
+        rows[i].style.display ='none';
+      }
+    }
+    this.time = false;
+  };
