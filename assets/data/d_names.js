7c4672921a1f05908cadfca48810ae57c1584b95
Jed Brazal <42727623+jbrazal@users.noreply.github.com>
2018-09-30 10:19:35 -0600

    Add d_names.js

---
+++ assets/data/d_names.js
@@ -0,0 +1,17 @@
+var d_names = [
+    {
+        name: "Drake",
+        read: "DReYK",
+        write: "D=ReY=K="
+    },
+    {
+        name: "Donald",
+        read: "DoNaLD",
+        write: "DoNaL=D="
+    },
+    {
+        name: "Daniel",
+        read: "DaNiYeL",
+        write: "DaNiYeL="
+    }
+    ];
