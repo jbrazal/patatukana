0cba45d669cb1dbbb8a3017ddd277834813bc9ed
Jed Brazal <42727623+jbrazal@users.noreply.github.com>
2018-09-30 10:24:10 -0600

    Add e_names.js

---
+++ assets/data/e_names.js
@@ -0,0 +1,18 @@
+var e_names = [
+    {
+        name: "Ernie",
+        read: "ERNi",
+        write: "E=R=Ni"
+    },
+    {
+        name: "Edward",
+        read: "EDuWaRD",
+        write: "EDuWaR=D="
+    }
+    ,
+    {
+        name: "Elmer",
+        read: "ELMeR",
+        write: "ELMeR="
+    }
+    ];
