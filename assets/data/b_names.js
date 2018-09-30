65e9c39cd2d2334d5631b8931c880862a8d6f570
Jed Brazal <42727623+jbrazal@users.noreply.github.com>
2018-09-30 10:19:17 -0600

    Add b_names.js

---
+++ assets/data/b_names.js
@@ -0,0 +1,17 @@
+var b_names = [
+    {
+        name: "Bob",
+        read: "BaB",
+        write: "BaB="
+    },
+    {
+        name: "Ben",
+        read: "BeN",
+        write: "BeN="
+    },
+    {
+        name: "Brandon",
+        read: "BRaNDoN",
+        write: "B=RaN=DoN="
+    }
+    ];
