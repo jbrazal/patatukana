5e5dad64c139a9cd9e73cf9942d1136b79fdfb32
Jed Brazal <42727623+jbrazal@users.noreply.github.com>
2018-09-30 10:19:26 -0600

    Add c_names.js

---
+++ assets/data/c_names.js
@@ -0,0 +1,17 @@
+var c_names = [
+    {
+        name: "Carlo",
+        read: "KaRLo",
+        write: "KaR=Lo"
+    },
+    {
+        name: "Catherine",
+        read: "Ka Te Ri N",
+        write: "KaTeRiN="
+    },
+    {
+        name: "Christine",
+        read: "KRiSTiN",
+        write: "K=RiS=TiN="
+    }
+    ];
