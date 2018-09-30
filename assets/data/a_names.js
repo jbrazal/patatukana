7ffc883ad9a5a514c8213a763faf8ca53df851c5
Jed Brazal <42727623+jbrazal@users.noreply.github.com>
2018-09-30 10:19:10 -0600

    Add a_names.js

---
+++ assets/data/a_names.js
@@ -0,0 +1,17 @@
+var a_names = [
+    {
+        name: "Arthur",
+        read: "A R Tu R",
+        write: "A=R=TuR="
+    },
+    {
+        name: "Abby",
+        read: "A Bi",
+        write: "ABi"
+    },
+    {
+        name: "Alan",
+        read: "A La N",
+        write: "ALa+N+"
+    }
+    ];
