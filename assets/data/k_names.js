4590e1790285753d336a80a53b7800d03143cebb
Jed Brazal <42727623+jbrazal@users.noreply.github.com>
2018-09-30 10:21:13 -0600

    Add k_names.js

---
+++ assets/data/k_names.js
@@ -0,0 +1,18 @@
+var k_names = [
+    {
+        name: "Kat",
+        read: "Ka t",
+        write: "Kat+"
+    },
+    {
+        name: "Karl",
+        read: "KaRL",
+        write: "KaR+L+"
+    }
+    ,
+    {
+        name: "KeN",
+        read: "KeN",
+        write: "KeN="
+    }
+    ];
