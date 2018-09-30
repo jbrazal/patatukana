d3d0234428816aecfeaa90a6741474deed8ecbb4
Jed Brazal <42727623+jbrazal@users.noreply.github.com>
2018-09-30 10:30:16 -0600

    Add h names

---
+++ assets/data/h_names.js
@@ -0,0 +1,18 @@
+var h_names = [
+    {
+        name: "Harrold",
+        read: "HaRoLD",
+        write: "HaRoL=D="
+    },
+    {
+        name: "Henry",
+        read: "HeNRi",
+        write: "HeN=Ri"
+    }
+    ,
+    {
+        name: "Hailey",
+        read: "HeYLi",
+        write: "HeY=Li"
+    }
+    ];
