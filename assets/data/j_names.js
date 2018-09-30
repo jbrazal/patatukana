a9c0919e4c194759e15377f57681becebb154727
Jed Brazal <42727623+jbrazal@users.noreply.github.com>
2018-09-30 10:20:28 -0600

    Add j_names.js

---
+++ assets/data/j_names.js
@@ -0,0 +1,18 @@
+var j_names = [
+    {
+        name: "Jed",
+        read: "D Ye D",
+        write: "D=YeD="
+    },
+    {
+        name: "John",
+        read: "D Yan",
+        write: "D=YaN="
+    }
+    ,
+    {
+        name: "Jack",
+        read: "DYaK",
+        write: "D=YaK="
+    }
+    ];
