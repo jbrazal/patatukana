e0195e9c9d1a53701063a2a8c08b1c845591490c
jbrazal <42727623+jbrazal@users.noreply.github.com>
2018-10-01 22:18:09 -0600

    Update k_names.js

--- assets/data/k_names.js
+++ assets/data/k_names.js
@@ -5,6 +5,11 @@ var k_names = [
         write: "Kat+"
     },
     {
+        name: "Katrina",
+        read: "katrina",
+        write: "kat=rina"
+    },
+    {
         name: "Karl",
         read: "KaRL",
         write: "KaR+L+"
@@ -14,5 +19,10 @@ var k_names = [
         name: "KeN",
         read: "KeN",
         write: "ken="
+    },
+    {
+        name: "Kit",
+        read: "kit",
+        write: "kit="
     }
     ];
