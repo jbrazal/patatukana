75ca7d4813a9d8a7286a6408c523d42ef41b72dc
Jed Brazal <42727623+jbrazal@users.noreply.github.com>
2018-10-03 08:01:48 -0600

    select default font

--- scripts/names.js
+++ scripts/names.js
@@ -3,7 +3,7 @@
 var app = angular.module('baybayin', [])
 .controller('ctrl', function ($scope) {
     $scope.Names = allNames;
-    $scope.chk_font1 = true;
+    $scope.chk_font4 = true;  //select default font
 });
 
 
